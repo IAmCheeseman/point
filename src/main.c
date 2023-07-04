6d1132b6d01d3ec77baf584a039eb8029b8e74db
iamcheeseman <xanderswan32@gmail.com>
2023-07-03 20:50:25 -0400

    Split up main

--- src/main.c
+++ src/main.c
@@ -4,33 +4,63 @@
 #include "engine.h"
 #include "luahelpers.h"
 
-int main(int argc, char* args[]) {
-    EngineState* engine = create_engine();
-
-    int res = luaL_dofile(engine->L, "main.lua");
-    if (res != LUA_OK) {
-        fprintf(stderr, "[LUA]:\n%s\n", lua_tostring(engine->L, -1));
-        return 1;
+void process_events(EngineState* engine) {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_QUIT: {
+                engine->window.should_close = true;
+                break;
+            }
+            default: {
+                handle_events(&engine->event_handler, engine->L, engine->traceback_location, event);
+                break;
+            }
+        }
     }
+}
 
-    call_lua_table_func(engine->L, engine->traceback_location, "point", "onload", 0);
+void process(EngineState* engine, double delta) {
+    lua_getglobal(engine->L, "point");
+    luaL_checktype(engine->L, -1, LUA_TTABLE);
 
+    lua_getfield(engine->L, -1, "onstep");
+    luaL_checktype(engine->L, -1, LUA_TFUNCTION);
+    lua_pushnumber(engine->L, delta);
+    call_lua_func(engine->L, engine->traceback_location, 1, 0);
+
+    lua_pop(engine->L, 1); // Table
+}
+
+void draw(EngineState* engine) {
+    // Start rendering
+    engine->renderer.is_rendering = true;
+    SDL_SetRenderTarget(engine->renderer.sdl_renderer, engine->renderer.target);
+
+    // Render game
+    SDL_SetRenderDrawColor(engine->renderer.sdl_renderer, 0, 0, 0, 255);
+    SDL_RenderClear(engine->renderer.sdl_renderer);
+    SDL_SetRenderDrawColor(engine->renderer.sdl_renderer, 255, 255, 255, 255);
+
+    call_lua_table_func(engine->L, engine->traceback_location, "point", "ondraw", 0);
+
+    SDL_SetRenderTarget(engine->renderer.sdl_renderer, NULL);
+    // Render target texture
+    SDL_SetRenderDrawColor(engine->renderer.sdl_renderer, 0, 0, 0, 255);
+    SDL_RenderClear(engine->renderer.sdl_renderer);
+
+    SDL_Rect dst = (SDL_Rect) { x, y, w, h };
+    SDL_RenderCopy(engine->renderer.sdl_renderer, engine->renderer.target, NULL, &dst);
+
+    SDL_RenderPresent(engine->renderer.sdl_renderer);
+    engine->renderer.is_rendering = false;
+}
+
+void main_loop(EngineState* engine) {
     double now = SDL_GetPerformanceCounter();
     double prev = 0;
     while (!engine->window.should_close) {
-        SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-                case SDL_QUIT: {
-                    engine->window.should_close = true;
-                    break;
-                }
-                default: {
-                    handle_events(&engine->event_handler, engine->L, engine->traceback_location, event);
-                    break;
-                }
-            }
-        }
+        process_events(engine);
         
         // Calculate where the target texture goes
         int ww, wh;
@@ -48,44 +78,28 @@ int main(int argc, char* args[]) {
         engine->event_handler.mouse_x = (mx - x) / scale;
         engine->event_handler.mouse_y = (my - y) / scale;
 
-        // Process game
-        lua_getglobal(engine->L, "point");
-        luaL_checktype(engine->L, -1, LUA_TTABLE);
-        
         // Why is dt always SO cringe to calculate!
         prev = now;
         now = SDL_GetPerformanceCounter();
         double delta = ((double)((now - prev) * 1000 / (double)SDL_GetPerformanceFrequency()) * 0.001);
 
-        lua_getfield(engine->L, -1, "onstep");
-        luaL_checktype(engine->L, -1, LUA_TFUNCTION);
-        lua_pushnumber(engine->L, delta);
-        call_lua_func(engine->L, engine->traceback_location, 1, 0);
-
-        lua_pop(engine->L, 1); // Table
-                
-        // Start rendering
-        engine->renderer.is_rendering = true;
-        SDL_SetRenderTarget(engine->renderer.sdl_renderer, engine->renderer.target);
-
-        // Render game
-        SDL_SetRenderDrawColor(engine->renderer.sdl_renderer, 0, 0, 0, 255);
-        SDL_RenderClear(engine->renderer.sdl_renderer);
-        SDL_SetRenderDrawColor(engine->renderer.sdl_renderer, 255, 255, 255, 255);
-
-        call_lua_table_func(engine->L, engine->traceback_location, "point", "ondraw", 0);
-
-        SDL_SetRenderTarget(engine->renderer.sdl_renderer, NULL);
-        // Render target texture
-        SDL_SetRenderDrawColor(engine->renderer.sdl_renderer, 0, 0, 0, 255);
-        SDL_RenderClear(engine->renderer.sdl_renderer);
-
-        SDL_Rect dst = (SDL_Rect) { x, y, w, h };
-        SDL_RenderCopy(engine->renderer.sdl_renderer, engine->renderer.target, NULL, &dst);
-
-        SDL_RenderPresent(engine->renderer.sdl_renderer);
-        engine->renderer.is_rendering = false;
+        process(engine);
+        draw(engine)
     }
+}
+
+int main(int argc, char* args[]) {
+    EngineState* engine = create_engine();
+
+    int res = luaL_dofile(engine->L, "main.lua");
+    if (res != LUA_OK) {
+        fprintf(stderr, "[LUA]:\n%s\n", lua_tostring(engine->L, -1));
+        return 1;
+    }
+
+    call_lua_table_func(engine->L, engine->traceback_location, "point", "onload", 0);
+
+    
 
     free_engine(engine);   
 
